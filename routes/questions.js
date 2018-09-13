3a6fcc51ad5ee79c2cc26ac3b34105318abf844b
William Holcombe <william.holcombe@uconn.edu>
2018-09-12 21:27:32 -0400

    space

--- routes/questions.js
+++ routes/questions.js
@@ -45,9 +45,64 @@ DothQ.enqueue({
 
 
 router.get('/', (req, res) => {
+
+
+  //User.findOne({ 'username': username }).then(() => {
+    
+//   //   if (cnt > 0) {
+//   //     const err = new Error('username already exists');
+//   //     err.status = 422;
+//   //     return next(err);
+//   //   } else { //else if no validation errors, create user
+    
+//   //     return User.hashPassword(password)
+//   //       .then(digest => { 
+//   //         encryptedPassword = digest;
+//   //         return Questions.find();
+//   //       })
+//   //       .then((questions) => {
+//   //         //console.log('concosle.log questions', questions);
+//   //         questions.map((question, index) => {
+//   //           //console.log('indeces', index, question);
+//   //           if(index !== questions.length -1 ){ 
+//   //             question._next = index +1;
+//   //           } else {
+//   //             question._next = 0;
+//   //           }
+            
+//   //         });
+//   //         //console.log('after next map', questions);
+//   //         const newUser = {
+//   //           username,
+//   //           password: encryptedPassword,
+//   //           firstname: firstName,
+//   //           lastName,
+//   //           questions,
+//   //           head: 0
+//   //         };
+//   //         return User.create(newUser);
+//   //       })
+//   //       .then(user => {
+//   //        // console.log(user, '>>>>>>>>>>>USER');
+//   //         return res.status(201).location(`/api/users/${user.id}`).json(user.serialize());
+//   //       })
+//   //       .catch(err => {
+//   //         if (err.code === 11000) {
+//   //           err = new Error('The username already exists');
+//   //           err.status = 400;
+//   //         }
+//   //         next(err);
+//   //       });
+//   //   }
+//   // });
+// });
+
+console.log(req.currentUser);
   const nextQuestion = DothQ.dequeue();
   DothQ.enqueue(nextQuestion);
   return res.json(nextQuestion);
+
+
 
 //   let questions = req.user.questions.sort((a,b)=>{
 //     return a.score - b.score;
